bdd58e6ee832bdff1a84686173569f803eed6323
WickyNilliams <WickyNilliams@MBA>
2014-08-27 11:26:46 +0100

    drop reflux dependency

--- lib/components/colorpicker.react.jsx
+++ lib/components/colorpicker.react.jsx
@@ -1,27 +1,42 @@
 var React = require('react');
-var Reflux = require('reflux');
-
-var store = require('../store');
-var actions = require('../actions');
+var Color = require('color');
 
 var Map = require('./map.react');
 var Slider = require('./slider.react');
 
 var ColorPicker = React.createClass({
 
-  mixins: [Reflux.ListenerMixin],
-
-  componentDidMount: function () {
-    this.listenTo(store, this.onChange);
+  getDefaultProps : function() {
+    return {
+      color : "#000000"
+    };
   },
 
-  onChange: function () {
-    this.forceUpdate();
+  getInitialState: function() {
+    return this.getStateFrom(this.props.color);
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    var state = this.getStateFrom(nextProps.color);
+
+    if(state.color.hexString() !== this.state.color.hexString()) {
+      this.setState(state);
+    }
+  },
+
+  getStateFrom : function(color) {
+    color = new Color(color);
+    return {
+      color : color,
+      raw : this.getRawHsv(color)
+    };
   },
 
   render: function () {
-    var rawHsv = store.toRawHsv();
-    var luminosity = store.toLum();
+    var color = this.state.color;
+    var rawHsv = this.state.raw;
+    var luminosity = color.luminosity();
+    var hue = this.toHue(color);
 
     var classes = React.addons.classSet({
       dark: luminosity <= 0.5,
@@ -42,7 +57,7 @@ var ColorPicker = React.createClass({
           x={rawHsv.s}
           y={rawHsv.v}
           className={classes}
-          backgroundColor={store.toHue()}
+          backgroundColor={hue}
           onChange={this.handleSaturationValueChange} 
         />
       </div>
@@ -50,13 +65,49 @@ var ColorPicker = React.createClass({
     );
   },
 
+  toHue : function(color) {
+    return Color().hsv(color.hue(), 100, 100).hexString();
+  },
+
+  getRawHsv : function(color) {
+    var hsv = color.hsv();
+    return {
+      h: hsv.h / 360,
+      s: hsv.s / 100,
+      v: hsv.v / 100
+    };
+  },
+
   handleHueChange : function(hue) {
-    actions.setHue(hue);
+    var color = this.state.color;
+    var raw = this.state.raw;
+    var oldColor = color.hexString();
+
+    raw.h = hue;
+    color.hue(hue * 360);
+
+    this.setState(this.state);
+
+    if(oldColor != color.hexString()) {
+      this.props.onChange(this.state.color.hexString());
+    }
   },
 
   handleSaturationValueChange : function(saturation, value) {
-    actions.setSaturation(saturation);
-    actions.setValue(value);
+    var color = this.state.color;
+    var raw = this.state.raw;
+    var oldColor = color.hexString();
+
+    color.saturationv(saturation * 100);
+    color.value(value * 100);
+    raw.s = saturation;
+    raw.v = value;
+
+    this.setState(this.state);
+
+    if(oldColor != color.hexString()) {
+      this.props.onChange(color.hexString());
+    }
   },
 
 });
